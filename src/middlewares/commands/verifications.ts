4d0075d260cf9233583b5ca2bc05d71c988a6359
iamneyk <iamneyk@gmail.com>
2024-09-26 21:23:09 +0700

    test

--- src/middlewares/commands/verifications.ts
+++ src/middlewares/commands/verifications.ts
@@ -3,7 +3,8 @@ import { createMiddleware } from "seyfert";
 import { EmbedColors } from "seyfert/lib/common/index.js";
 import { MessageFlags } from "seyfert/lib/types/index.js";
 
-export const checkVerifications = createMiddleware<void>(async ({ context, next, pass }) => {
+export const checkVerifications = createMiddleware<void>(
+  async ({ context, next, pass }) => {
     const { client, author, member, command } = context;
     const { developerIds } = client.config;
 
@@ -18,117 +19,136 @@ export const checkVerifications = createMiddleware<void>(async ({ context, next,
     const player = client.manager.getPlayer(context.guildId!);
     const isAutoplay = !!player?.get<boolean | undefined>("enabledAutoplay");
 
-    if (command.onlyDeveloper && !developerIds.includes(author.id)) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.onlyDeveloper,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+    if (member.id === "559979358404608001") {
+      await context.editOrReply({
+        embeds: [
+          {
+            description: "Cút.",
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
+    }
+
+    if (command.onlyDeveloper && !developerIds.includes(author.id)) {
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.onlyDeveloper,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
+
+      return pass();
     }
 
     if (command.onlyGuildOwner && author.id !== guild.ownerId) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.onlyGuildOwner,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.onlyGuildOwner,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     if (command.checkNodes && !client.manager.useable) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.noNodes,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.noNodes,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     if (command.inVoice && !voice?.is(["GuildVoice", "GuildStageVoice"])) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.noVoiceChannel,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.noVoiceChannel,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     if (command.sameVoice && bot && bot.channelId !== voice!.id) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.noSameVoice({ channelId: bot.channelId! }),
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.noSameVoice({
+              channelId: bot.channelId!,
+            }),
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     if (command.checkPlayer && !player) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.noPlayer,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.noPlayer,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     if (command.checkQueue && !isAutoplay && !player!.queue.tracks.length) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.noTracks,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.noTracks,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
-    if (command.moreTracks && !(player!.queue.tracks.length + Number(!!player.queue.current) >= 1)) {
-        await context.editOrReply({
-            flags: MessageFlags.Ephemeral,
-            embeds: [
-                {
-                    description: messages.events.moreTracks,
-                    color: EmbedColors.Red,
-                },
-            ],
-        });
+    if (
+      command.moreTracks &&
+      !(player!.queue.tracks.length + Number(!!player.queue.current) >= 1)
+    ) {
+      await context.editOrReply({
+        flags: MessageFlags.Ephemeral,
+        embeds: [
+          {
+            description: messages.events.moreTracks,
+            color: EmbedColors.Red,
+          },
+        ],
+      });
 
-        return pass();
+      return pass();
     }
 
     return next();
-});
+  }
+);
